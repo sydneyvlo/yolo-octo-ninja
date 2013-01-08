79ade54c30e3a104fc10f928777268649c22ce27
Sydney Lo <sydneyvlo@gmail.com>
2013-01-08 11:50:14 -0800

    Created function prototypes and finished the prompts to get user input

--- ticketCalculator/ticketCalculator.cpp/ticketCalculator.cpp/Source.cpp
+++ ticketCalculator/ticketCalculator.cpp/ticketCalculator.cpp/Source.cpp
@@ -2,9 +2,15 @@
 #include <string>
 using namespace std;
 
+int calcRPrice(double &tPrice, double &tRPrice, double &ROI);
+int calcTAmount(int &tAmount, double &tPrice, double &tRPrice, double &ROI);
+double calcROI(double &tPrice, double &tRPrice);
+
 int main() {
-	double tPrice = 0.0, tRPrice = 0.0, rOi = 0.0;
+	double tPrice = 0.0, tRPrice = 0.0, ROI = 0.0;
 	int tAmount = 0, choice;
+	bool again = false;
+	string repeat;
 
 	cout << "Menu\n" 
 		<< endl 
@@ -21,4 +27,22 @@ int main() {
 	
 	cout << "Choose 1, 2, or 3: " << flush;
 	cin >> choice;
+
+	do {
+		if (choice == 1) {
+			calcRPrice();
+		} else if (choice == 2) {
+			calcTAmount(); 
+		} else {
+			calcROI();
+		}
+
+		cout << "Would you like to calculate anything else? (y/n)" << endl;
+		getline(cin, repeat);
+		if (repeat == "y") {
+			again == true;
+		} else {
+			again == false;
+		}
+	} while (again);
 }
