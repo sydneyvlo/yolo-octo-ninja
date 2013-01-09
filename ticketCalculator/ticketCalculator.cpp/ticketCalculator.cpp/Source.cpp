b3c48b2840b73854a342f86156df56e6f708afc6
Sydney Lo <sydneyvlo@gmail.com>
2013-01-08 18:11:39 -0800

    Created a prompt function that displays the menu and gets the input from the user.

--- ticketCalculator/ticketCalculator.cpp/ticketCalculator.cpp/Source.cpp
+++ ticketCalculator/ticketCalculator.cpp/ticketCalculator.cpp/Source.cpp
@@ -3,6 +3,7 @@
 #include <string>
 using namespace std;
 
+void prompt(int &choice);
 void calcRPrice(double &tPrice, double &tRPrice, double &ROI);
 void calcTAmount(int &tAmount, double &tPrice, double &tRPrice, double &ROI, double &profit);
 void calcROI(double &tPrice, double &tRPrice);
@@ -12,26 +13,11 @@ int main() {
 	int tAmount = 0, choice;
 	bool again = false;
 	char repeat;
-
-	cout << "Menu\n" 
-		<< endl 
-		<< "1. Calculate the resell price needed to \
-		   \nmatch return on investment percentage. \n"
-		<< endl		
-		<< "2. Calculate the amount of tickets needed to \
-		   \nmeet a desired profit at a certain return on \
-		   \ninvestment based on the original ticket price and resell price.\n" 
-		<< endl 
-		<< "3. Calculate the return on investment based \
-		   \noriginal ticket price and ticket resell price.\n" 
-		<< endl;
 	
 	// Lets turn the prompt up there into a function. So we can repeatedly call it in the do while loop.
 
-	cout << "Choose 1, 2, or 3: " << flush;
-	cin >> choice;
-
 	do {
+		prompt(choice);
 		if (choice == 1) {
 			calcRPrice(tPrice, tRPrice, ROI);
 		} else if (choice == 2) {
@@ -40,14 +26,33 @@ int main() {
 //			calcROI();
 		}
 
-		cout << "Would you like to calculate anything else? (y/n)" << endl;
+		cout << "Would you like to calculate anything else? (y/n)" << flush;
 		cin >> repeat;
 		if (repeat == 'y') {
 			again = true;
 		} else {
 			again = false;
+			cout << "See ya next time." << endl;
 		}
 	} while (again);
+}
+
+void prompt(int &choice) {
+	cout << "Menu\n" 
+	<< endl 
+	<< "1. Calculate the resell price needed to \
+		  \nmatch return on investment percentage. \n"
+	<< endl		
+	<< "2. Calculate the amount of tickets needed to \
+		  \nmeet a desired profit at a certain return on \
+		  \ninvestment based on the original ticket price and resell price.\n" 
+	<< endl 
+	<< "3. Calculate the return on investment based \
+		  \noriginal ticket price and ticket resell price.\n" 
+	<< endl << endl;
+
+	cout << "Choose 1, 2, or 3: " << flush;
+	cin >> choice;
 }
 
 void calcRPrice(double &tPrice, double &tRPrice, double &ROI) {
