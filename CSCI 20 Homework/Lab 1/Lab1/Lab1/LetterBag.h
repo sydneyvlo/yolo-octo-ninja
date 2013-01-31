0db47ee2293b5624443f2bd099fd162dcba2476c
Sydney Lo <sydneyvlo@gmail.com>
2013-01-30 17:13:51 -0800

    Turned the classes header file into a template class.

---
+++ CSCI 20 Homework/Lab 1/Lab1/Lab1/LetterBag.h
@@ -0,0 +1,16 @@
+#ifndef LETTERBAG_H 
+#define LETTERBAG_H
+
+// This letter bag 
+
+// This is so we can use this bag with multiple data types
+// This indicates that this is a template definition
+template <class ItemType> 
+
+class LetterBag {
+
+};
+
+// Include the implementation file.
+#include "LetterBag.cpp"
+#endif
