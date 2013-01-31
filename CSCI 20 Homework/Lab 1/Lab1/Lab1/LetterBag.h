ba0ddd2f71dc887954699f7daaf112b33b4b2b2d
Sydney Lo <sydneyvlo@gmail.com>
2013-01-30 17:36:54 -0800

    Added comments to the class header file.

--- CSCI 20 Homework/Lab 1/Lab1/Lab1/LetterBag.h
+++ CSCI 20 Homework/Lab 1/Lab1/Lab1/LetterBag.h
@@ -8,6 +8,48 @@
 template <class ItemType> 
 
 class LetterBag {
+	public:
+
+		// Constructor. Takes in no arguments. Will create the bag
+		LetterBag();
+
+		// Put the letters from the vector v into the bag.
+		LetterBag(const vector<char> &v);
+
+		// Checks to see if the two bags contain the same content. No more no less.
+		bool LetterBag operator==(const LetterBag &other) const;
+
+		// Is true if the two bags are not identical
+		bool LetterBag operator!=(const LetterBag &other) const;
+
+		// Returns the current size of the bag.
+		int getCurrrentSize() const;
+
+		// Returns true if the bag is empty. False if it is not.
+		bool isEmpty() const;
+
+		// Adds a character to the bag.
+		LetterBag &add(char c);
+
+		// Removes a character from the bag
+		// pre condition: The item has to be present in the bag.
+		// post condition: One instance of the item is removed.
+		LetterBag &remove(char c);
+		
+		// Empties out the bag.
+		//pre conditon: Non
+		//post condition: Empty bag
+		LetterBag &clear();
+
+		// Returns the number of times that an item appears in the bag.
+		int getFrequency(char c) const;
+
+		// Returns a vector that is a duplicate of the bag.(Which in our case is an array).
+		vector<char> toVector() const;
+
+	private:
+		ItemType Bag[26];
+
 
 };
 
