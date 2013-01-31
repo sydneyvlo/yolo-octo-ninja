f63e88305f0d7814863aadc77e18b828fd5dbba5
Sydney Lo <sydneyvlo@gmail.com>
2013-01-30 22:27:46 -0800

    Program compiles after removing commented out methods. Fixed Traugotts error.

--- CSCI 20 Homework/Lab 1/Lab1/Lab1/LetterBag.h
+++ CSCI 20 Homework/Lab 1/Lab1/Lab1/LetterBag.h
@@ -18,35 +18,35 @@ class LetterBag {
 		LetterBag(const  std::vector<char> &v); // Why in the world did I need to use std::vector instead of vector.
 
 		// Checks to see if the two bags contain the same content. No more no less.
-//		bool LetterBag operator==(const LetterBag &other) const;
+		bool operator==(const LetterBag &other) const;
 
 		// Is true if the two bags are not identical
-//		bool LetterBag operator!=(const LetterBag &other) const;
+		bool operator!=(const LetterBag &other) const;
 
 		// Returns the current size of the bag.
-//		int getCurrrentSize() const;
+		int getCurrrentSize() const;
 
 		// Returns true if the bag is empty. False if it is not.
-//		bool isEmpty() const;
+		bool isEmpty() const;
 
 		// Adds a character to the bag.
-//		LetterBag &add(char c);
+		LetterBag &add(char c);
 
 		// Removes a character from the bag
 		// pre condition: The item has to be present in the bag.
 		// post condition: One instance of the item is removed.
-//		LetterBag &remove(char c);
+		LetterBag &remove(char c);
 		
 		// Empties out the bag.
 		//pre conditon: Non
 		//post condition: Empty bag
-//		LetterBag &clear();
+		LetterBag &clear();
 
 		// Returns the number of times that an item appears in the bag.
-//		int getFrequency(char c) const;
+		int getFrequency(char c) const;
 
 		// Returns a vector that is a duplicate of the bag.(Which in our case is an array).
-//		vector<char> toVector() const;
+		 std::vector<char> toVector() const;
 
 	private:
 		int Bag[27];
@@ -54,6 +54,6 @@ class LetterBag {
 
 };
 
-// Include the implementation file.
+// #include "LetterBag.cpp"; // Include the implementation file.
 //#include "LetterBag.cpp"
 #endif
