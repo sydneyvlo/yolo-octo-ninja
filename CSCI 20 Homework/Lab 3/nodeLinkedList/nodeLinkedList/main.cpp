99b5e58502488923324833aec4a65508dc12d71e
Sydney Lo <sydneyvlo@gmail.com>
2013-02-21 20:52:21 -0800

    Fix errors in createNode, insertVal, and removeVal. Wrote the print function. About to write and test the previously mentioned functions.

--- CSCI 20 Homework/Lab 3/nodeLinkedList/nodeLinkedList/main.cpp
+++ CSCI 20 Homework/Lab 3/nodeLinkedList/nodeLinkedList/main.cpp
@@ -7,35 +7,77 @@ struct node {
 };
 
 node* createNode(int val = 0) {
-	node temp;
-	temp.info = val;
-	temp.link = NULL;
-	return &temp;
+	node *temp;
+	temp = new node;
+	temp->info = val;
+	return temp;
 }
 
+//first should not be null.
 node* insertVal(node *first, int val) {
-	node temp;
-	temp.info = val;
-	temp.link = first->link;
-	first->link = &temp;
+	int count = 0;
+	bool inserted = false;
+
+	node *toBeAdded;
+	toBeAdded = new node;
+	toBeAdded->info = val;
+
+	//First node excluding the head. Aliases not creating new nodes.
+	node *placeInList = first->link;
+
+	//Also first node exluding the head. Aliases not creating new nodes.
+	node *prevInList = first->link;
+	
+	while (placeInList->link != NULL && !inserted) {
+		if (val <= placeInList->info && count == 0) {
+			first->link = toBeAdded;
+			toBeAdded->link = placeInList;
+			inserted = true;
+		} else if (val <= placeInList->info) {
+			toBeAdded->link = placeInList->link;
+			placeInList->link = toBeAdded;
+			inserted = true;
+		}
+		prevInList = placeInList;
+		placeInList = placeInList->link;
+		count++;
+	}
+
+	if (val <= placeInList->info && !inserted) {
+		toBeAdded->link = prevInList->link;
+		prevInList->link = toBeAdded;
+	} else if (!inserted) {
+		placeInList->link = toBeAdded;
+		toBeAdded->link = NULL;
+	}
+
 	return first;
+
 }
 
 node* removeVal(node* first, int val, int &numRemoved) {
-	numRemoved = 0;
-	node *prevInList = first;
-	node *placeInList = first;
+	int count = 0;
+	node *prevInList = first->link;
+	node *placeInList = first->link;
 	node *toBeDeleted;
 	while (placeInList->link != NULL) {
-		if (placeInList->info == val) {
-			//prevInList = placeInList->link;
+		if (placeInList->info == val && count == 0) {
+			// Since the first node to be deleted
+			// is the first node after the header, we make the headptr (first) point
+			// to the second node after the head.
+			first->link = placeInList->link;
 			toBeDeleted = placeInList;
-			if (first == placeInList) {
-				first = placeInList->link;
-			}
 			placeInList = placeInList->link;
 			delete toBeDeleted;
+			numRemoved++;
+		} else if (placeInList->info == val) {
+			prevInList->link = placeInList->link;
+			toBeDeleted = placeInList;
+			placeInList = placeInList->link;
+			delete toBeDeleted;
+			numRemoved++;
 		} else {
+			count++;
 			prevInList = placeInList;
 			placeInList = placeInList->link;
 		}
@@ -45,22 +87,19 @@ node* removeVal(node* first, int val, int &numRemoved) {
 	if (placeInList->info == val) {
 		prevInList->link = NULL;
 	}
+
+	return first;
 }
 
+void print (node* first) {
+	node* placeInList = first->link;
+	while (placeInList->link != NULL) {
+		cout << "{" << placeInList->info << "}, " << flush;
+		placeInList = placeInList->link;
+	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+	cout << "{" << placeInList->info << "}" << endl;
+}
 
 int main() {
 
