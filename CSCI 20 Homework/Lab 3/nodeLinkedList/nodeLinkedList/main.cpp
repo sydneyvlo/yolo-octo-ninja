99af9b88b68480cd69b1f7c93d6732500970106e
Sydney Lo <sydneyvlo@gmail.com>
2013-02-21 22:33:23 -0800

    stopped the program from crashing.

--- CSCI 20 Homework/Lab 3/nodeLinkedList/nodeLinkedList/main.cpp
+++ CSCI 20 Homework/Lab 3/nodeLinkedList/nodeLinkedList/main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 struct node {
-	int info ;
+	int info;
 	node *link;
 };
 
@@ -18,36 +18,49 @@ node* insertVal(node *first, int val) {
 	int count = 0;
 	bool inserted = false;
 
+	cout << "1" << endl;
 	node *toBeAdded;
 	toBeAdded = new node;
 	toBeAdded->info = val;
 
+	cout << "2" << endl;
 	//First node excluding the head. Aliases not creating new nodes.
 	node *placeInList = first->link;
 
+	cout << "3" << endl;
 	//Also first node exluding the head. Aliases not creating new nodes.
 	node *prevInList = first->link;
 	
-	while (placeInList->link != NULL && !inserted) {
-		if (val <= placeInList->info && count == 0) {
-			first->link = toBeAdded;
-			toBeAdded->link = placeInList;
-			inserted = true;
-		} else if (val <= placeInList->info) {
-			toBeAdded->link = placeInList->link;
-			placeInList->link = toBeAdded;
-			inserted = true;
-		}
-		prevInList = placeInList;
-		placeInList = placeInList->link;
-		count++;
-	}
+	cout << "4" << endl;
 
-	if (val <= placeInList->info && !inserted) {
-		toBeAdded->link = prevInList->link;
-		prevInList->link = toBeAdded;
-	} else if (!inserted) {
-		placeInList->link = toBeAdded;
+	// If this fails that means the linked list was empty. I just consisted of a headPtr;
+	if (placeInList != NULL) {
+		while (placeInList->link != NULL && !inserted) {
+		cout << "5x" << endl;
+			if (val <= placeInList->info && count == 0) {
+				first->link = toBeAdded;
+				toBeAdded->link = placeInList;
+				inserted = true;
+			} else if (val <= placeInList->info) {
+				toBeAdded->link = placeInList->link;
+				placeInList->link = toBeAdded;
+				inserted = true;
+			}
+			prevInList = placeInList;
+			placeInList = placeInList->link;
+			count++;
+		}
+
+		cout << "5" << endl;
+		if (val <= placeInList->info && !inserted) {
+			toBeAdded->link = prevInList->link;
+			prevInList->link = toBeAdded;
+		} else if (!inserted) {
+			placeInList->link = toBeAdded;
+			toBeAdded->link = NULL;
+		}
+	} else {
+		first->link = toBeAdded;
 		toBeAdded->link = NULL;
 	}
 
@@ -92,8 +105,11 @@ node* removeVal(node* first, int val, int &numRemoved) {
 }
 
 void print (node* first) {
+	cout << "Greetings from the print function." << endl;
 	node* placeInList = first->link;
+	cout << "We make it here" << endl;
 	while (placeInList->link != NULL) {
+		cout << "Greetings from the while loop in the print function." << endl;
 		cout << "{" << placeInList->info << "}, " << flush;
 		placeInList = placeInList->link;
 	}
@@ -102,5 +118,11 @@ void print (node* first) {
 }
 
 int main() {
+	node* testNode;
+	testNode = new node;
+	testNode->link = NULL;
 
+	insertVal(testNode, 5);
+	cout << "Before the print function call." << endl;
+	print(testNode);
 }
