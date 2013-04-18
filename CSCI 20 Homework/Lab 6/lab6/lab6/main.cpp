f7c4d60afb75012a4e625a935659186ef6c4d9e4
Sydney Lo <sydneyvlo@gmail.com>
2013-04-18 15:17:51 -0700

    Removed unnecessary code from the main.
    The only thing I really want is the makeRandomTree function,addTree, and
    levelOrder functins.

--- CSCI 20 Homework/Lab 6/lab6/lab6/main.cpp
+++ CSCI 20 Homework/Lab 6/lab6/lab6/main.cpp
@@ -19,19 +19,19 @@ void inorderTraversal(node  * p);   // print node data in order
 void preorderTraversal(node  * p);  // print node data pre order
 void postorderTraversal(node * p);  // print node data post order
 void levelorderTraversal(node * p); // level order
-int leafCount(node * p);  // return number of leaves in p
-int height(node * p);  // return height of p
-string longestPath(node * p);  // most number of edges between p and a leaf
-string shortestPath(node * p); // least number of edges between p and a leaf
-int nodeCount(node * p);    // return number of nodes in p
+//int leafCount(node * p);  // return number of leaves in p
+//int height(node * p);  // return height of p
+//string longestPath(node * p);  // most number of edges between p and a leaf
+//string shortestPath(node * p); // least number of edges between p and a leaf
+//int nodeCount(node * p);    // return number of nodes in p
 
 node * makeARandomTree(int maxHeight); // This one is written for you.
 
-void deleteTree(node * p);  // delete all dynamically allocated memory for this tree
-void drawTree(node * p, bool condense);  // draw the tree. Use condensed spacing if condense == true.
+//void deleteTree(node * p);  // delete all dynamically allocated memory for this tree
+//void drawTree(node * p, bool condense);  // draw the tree. Use condensed spacing if condense == true.
 node *  addTrees(node * leftT, node * rightT, char c);   // create a new tree with root c.
-bool isFull(node * p) ; // return true iff p is a full tree
-bool isComplete(node * p) ;  // return true iff p is a complete tree
+//bool isFull(node * p) ; // return true iff p is a full tree
+//bool isComplete(node * p) ;  // return true iff p is a complete tree
 
 
 
@@ -41,17 +41,6 @@ int main() {
     cin >> seed;
     srand(seed >= 0 ? seed : (unsigned int)(time(0))); 
 
-    cout << "Execute memory leak test? (y/n): ";
-    char response;
-    cin >> response;
-    if (response == 'y') {
-        cout << "Starting memory leak test " << endl;
-        for (int i = 0; i < 50; i++) {       
-            node * p = makeARandomTree(30);    
-            deleteTree(p); 
-        }
-        cout << "Done with memory leak test " << endl;
-    }
     bool done = false;
     while (!done) {
         int maxHeight;
@@ -61,28 +50,28 @@ int main() {
             done = true;
  else {  
           node * p = makeARandomTree(maxHeight);
-          drawTree(p, maxHeight > 4);  // used condensed spacing if maxHeight > 5
+//         drawTree(p, maxHeight > 4);  // used condensed spacing if maxHeight > 5
  
           cout << endl;
-          cout << "\tHeight = " << height(p ) << ", Leafcount = "
-               << leafCount(p) <<  ", NodeCount = " << nodeCount(p) << endl;
+//          cout << "\tHeight = " << height(p ) << ", Leafcount = "
+//               << leafCount(p) <<  ", NodeCount = " << nodeCount(p) << endl;
  
-          cout <<    endl << "\tPre Order:     < ";
-          preorderTraversal(p);
-          cout << ">" << endl << endl << "\tIn Order:      < ";
-          inorderTraversal(p);
-          cout << ">" << endl <<endl <<  "\tPost Order:    < ";
-          postorderTraversal(p);
+//          cout <<    endl << "\tPre Order:     < ";
+//          preorderTraversal(p);
+//          cout << ">" << endl << endl << "\tIn Order:      < ";
+//          inorderTraversal(p);
+//          cout << ">" << endl <<endl <<  "\tPost Order:    < ";
+//          postorderTraversal(p);
           cout << ">" << endl <<endl <<  "\tLevel Order:   < ";
-          levelorderTraversal(p);
+//          levelorderTraversal(p);
           cout << ">" << endl << endl;
  
-          cout << "\tShortest Path: < " << shortestPath(p) << ">" << endl << endl;
-          cout << "\tLongest Path:  < " << longestPath(p)   << ">" << endl << endl;    
-          cout << "\tThis tree " << (isFull(p) ? "is" : "is not" ) << " full. " << endl << endl;
-          cout << "\tThis tree " << (isComplete(p) ? "is" : "is not" ) << " complete. " << endl << endl;        
+//          cout << "\tShortest Path: < " << shortestPath(p) << ">" << endl << endl;
+//          cout << "\tLongest Path:  < " << longestPath(p)   << ">" << endl << endl;    
+//          cout << "\tThis tree " << (isFull(p) ? "is" : "is not" ) << " full. " << endl << endl;
+//          cout << "\tThis tree " << (isComplete(p) ? "is" : "is not" ) << " complete. " << endl << endl;        
  
-          deleteTree(p);
+//          deleteTree(p);
        }  
     }
     cout << "Bye!" << endl;
@@ -98,4 +87,17 @@ node * makeARandomTree(int height) {
     node * q = rand() % 5 != 0 ? makeARandomTree(height-1) : NULL;
  
     return addTrees(p,q, char('A' + rand() % ('Z' - 'A' + 1)));
+}
+
+void preorderTraversal(node *p) {
+	cout << p->info << flush;
+	preorderTraversal(p->left);
+	preorderTraversal(p->right);
+}
+
+
+void postorderTraversal(node *p) {
+	postorderTraversal(p->right);
+	postorderTraversal(p->left);
+	cout << p->info << flush;
 }
