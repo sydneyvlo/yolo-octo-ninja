8d0a9dab1f4b9a32cc28ca76773fb6e63b073148
Sydney Lo <sydneyvlo@gmail.com>
2013-05-21 02:46:55 -0700

    Wrote down the methods of both classes (dictionary and anagramset)
    I went through the tester cpp and recognized which methods were being
    used and made note of it in a notepad++ file.

--- CSCI 20 Homework/PA#4/PA4/PA4/AnagramSet.h
+++ CSCI 20 Homework/PA#4/PA4/PA4/AnagramSet.h
@@ -1,12 +1,21 @@
 #ifndef ANAGRAM_SET_H
 #define	ANAGRAM_SET_H
 
-class AnagramSet {
+#include <string>
+#include <iostream>
+using namespace std;
+
+class AnagramSet: public Dictonary {
 	friend ostream &operator<<(ostream &out, AnagramSet obj);
 
 public:
 
+	// Constructor 1
+	AnagramSet(string key);
+	int getSize();
+
 private:
+
 }
 
 #endif;
--- CSCI 20 Homework/PA#4/PA4/PA4/Dictionary.h
+++ CSCI 20 Homework/PA#4/PA4/PA4/Dictionary.h
@@ -1,15 +1,26 @@
 #ifndef DICTIONARY_H
 #define DICTIONARY_H
 
+#include "AnagramSet.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
 class Dictionary {
 	
 	friend ostream &operator<<(ostream &out, Dictionary obj);
 	
 public:
-		
-		// Constructor
+		// Constructor 1
 		Dictionary(string words[], int numWords, int tableSize);
-	
+
+		// Constructor 2
+		Dictionary(int tableSize);
+
+		bool contains(string word);
+		void insert(string word);
+		void remove(string word);
+		AnagramSet getSet(string word);
 
 private:
 
